357c63214ebc35841a18ca031e6af35d605492f8
Welles Robinson <wir963@gmail.com>
2020-09-29 12:17:01 -0400

    fix for quoting se manifest file

--- src/generate_se_manifest_file.py
+++ src/generate_se_manifest_file.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import csv
 
 df = pd.read_csv(snakemake.input[0], sep="\t")
 df = df.loc[(df.plate == snakemake.wildcards["plate"]) & (df["sample"] == snakemake.wildcards["sample"]) & (df.patient == snakemake.wildcards["patient"])]
 df["fq1"] = df.apply(lambda x: "FASTQ/trimmed/{}-{}-{}_3.fastq.gz".format(x["patient"], x["sample"], x["cell"]), axis=1)
 df["fq2"] = "-"
-df["RG"] = df.apply(lambda x: "ID:{}\tPL:illumina\tSM:{}\tLB:RNA".format(x["cell"], x["cell"]), axis=1)
-df[["fq1", "fq2", "RG"]].to_csv(snakemake.output[0], sep="\t", index=False, header=False)
+df["RG_ID"] = df["cell"].apply(lambda x: "ID:{}".format(x))
+df["RG_PL"] = "PL:illumina"
+df["RG_SM"] = df["cell"].apply(lambda x: "SM:{}".format(x))
+df["RG_LB"] = "LB:RNA"
+df[["fq1", "fq2", "RGID", "RG_PL", "RG_SM", "RG_LB"]].to_csv(snakemake.output[0], sep="\t", index=False, header=False, quoting=csv.QUOTE_NONE)
